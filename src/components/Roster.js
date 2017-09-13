33044b8d89793863ca1da653e06ecf22a6ac4c82
Oliver Weber <dr.weberdd@gmail.com>
2017-09-13 23:04:59 +0200

    naming consistency

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import FullRoster from './FullRoster'
-import Player from './Developer'
+import Developer from './Developer'
 
 // The Roster component matches one of two different routes
 // depending on the full pathname
 const Roster = () => (
   <Switch>
     <Route exact path='/roster' component={FullRoster}/>
-    <Route path='/roster/:number' component={Player}/>
+    <Route path='/roster/:number' component={Developer}/>
   </Switch>
 )
 
